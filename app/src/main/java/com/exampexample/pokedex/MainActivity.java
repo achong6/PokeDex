7b8d06e1ba846a542c72ef23a17e4474f6e5adf3
erikly <erikly2@illinois.edu>
2020-05-02 15:32:15 -0500

    updated layout of mainActivity screen

--- app/src/main/java/com/exampexample/pokedex/MainActivity.java
+++ app/src/main/java/com/exampexample/pokedex/MainActivity.java
@@ -1,11 +1,12 @@
 package com.exampexample.pokedex;
-package com.codinginflow.multipleonclicklistenerexample;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -16,13 +17,11 @@ import com.android.volley.toolbox.Volley;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import org.json.JSONObject;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    /**
-     * a RequestQueue for the API.
-     */
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    /** A RequestQueue for the API. */
     private static RequestQueue queue;
     /** The variable for the button. */
     private Button button;
